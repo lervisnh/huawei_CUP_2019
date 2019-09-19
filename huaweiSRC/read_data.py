59a06d10deb113a08eb3c64b101a9a28480b22b3
Le. Li <lervisnh@gmail.com>
2019-09-19 20:26:17 +0800

    Update read_data.py
    files_split_rate默认为0.05

--- huaweiSRC/read_data.py
+++ huaweiSRC/read_data.py
@@ -3,23 +3,13 @@
 Created on Thu Sep 19 11:07:22 2019
 @author: lervisnh
 """
-'''
-train_set_files = os.listdir(os.getcwd()) #得到train_set文件夹下的所有文件名称
-for index, data_file in enumerate(train_set_files):
-    file_path = train_set_path+'/'+data_file
-    if not index:
-        total_data = pd.read_csv( file_path )
-    else:
-        total_data = pd.concat( [total_data, pd.read_csv( file_path )], axis=0 )
-'''
-
 import os
 import numpy as np
 import pandas as pd
 import random
 
-class input_data_generator:
-    def __init__(self, data_files_path, files_split_rate=.11):
+class batch_data_generator:
+    def __init__(self, data_files_path, files_split_rate=.05):
         try:
             if os.path.exists(data_files_path): #train数据集存放地址
                 os.chdir( data_files_path )
@@ -45,27 +35,33 @@ class input_data_generator:
                     file_name = self.files_name.pop(index)
                     with open(self.data_files_path +'/'+ file_name, 'r') as data_file:
                         a_file = pd.read_csv( data_file )
+                        ####################
                         #这里可以加特征预处理
+                        ####################
                         col_names = list(a_file)
                         inputs.append( a_file[[ _ for _ in col_names[0:-1]  ]] )
                         labels.append( a_file['RSRP'] )
                 yield (np.concatenate(inputs), np.concatenate(labels))
             #（剩余）文件数量不足每次迭代文件数量，则全部取出
             else: 
-                while self.files_name:
+                while self.files_name: #读取所有剩余的文件
                     file_name = self.files_name.pop(-1)
                     with open(self.data_files_path +'/'+ file_name, 'r') as data_file:
                         a_file = pd.read_csv( data_file )
+                        ####################
                         #这里可以加特征预处理
+                        ####################
                         col_names = list(a_file)
                         inputs.append( a_file[[ _ for _ in col_names[0:-1]  ]] )
                         labels.append( a_file['RSRP'] )
                 yield (np.concatenate(inputs), np.concatenate(labels))
         
-        
-        
-if __name__=='__main__':
+    def create_features(self, ):
+        pass
+    
 
+
+if __name__=='__main__':
     codes_path = os.getcwd() #代码存放地址
     father_path = os.path.abspath(os.path.join(os.getcwd(), "..")) #上一级目录
     train_set_path = father_path+'/train_set'
@@ -75,11 +71,11 @@ if __name__=='__main__':
             print('In train_set directory!')
     except:
         print('Check train_set directory!')
-    input_data_generator = input_data_generator(train_set_path)
+    batch_data_generator = batch_data_generator(train_set_path, files_split_rate=.05)
     
     n = 1
-    for x, y in input_data_generator.batch_generator() :
+    for x, y in batch_data_generator.batch_generator() :
         print(n)
         print(x.shape)
         print(y.shape)
-        n+=1 
+        n+=1
